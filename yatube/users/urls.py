f65838b9fa76261f59b89d64d5fdce92b4063ebb
Ольга Жолудева <joludeva.olga@yandex.ru>
2022-09-22 19:41:57 +0300

    Внесены правки по итогам 1-го ревью и загрузки

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -8,18 +8,20 @@ app_name = 'users'
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path(
-        'logout/', 
-        LogoutView.as_view(template_name='users/logged_out.html'), 
+        'logout/',
+        LogoutView.as_view(template_name='users/logged_out.html'),
         name='logout'
     ),
     path(
-        'login/', 
-        LoginView.as_view(template_name='users/login.html'), 
+        'login/',
+        LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
     path(
-        'password_reset/', 
-        PasswordResetView.as_view(template_name='users/password_reset_form.html'), 
+        'password_reset/',
+        PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'
+        ),
         name='password_reset'
     ),
 ]
